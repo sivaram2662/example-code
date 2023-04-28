dc39a0af3a0a796ec9966e9097d2ab4b82952903
sivaram2662 <106672296+sivaram2662@users.noreply.github.com>
2023-04-28 20:40:07 +0530

    golang

--- webapp.go
+++ webapp.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
 )
 
-func HomeEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello world :)")
-}
-
-func main() {
-	http.HandleFunc("/", HomeEndpoint)
-	if err := http.ListenAndServe(":3000", nil); err != nil {
-		log.Fatal(err)
+func handler(w http.ResponseWriter, r *http.Request) {
+	html, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Unable to read file: %v", err), http.StatusInternalServerError)
+		return
 	}
+	fmt.Fprintf(w, "%s", html)
+}
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("starting the server on :3000...")
+	http.ListenAndServe(":3000", nil)
 }
